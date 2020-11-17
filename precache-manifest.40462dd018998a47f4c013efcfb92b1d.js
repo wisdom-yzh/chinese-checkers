de7214a1583f0f7d1f7c7c9dd66ca46fcfa5de87
wisdom-yzh <wisdom-yzh@users.noreply.github.com>
2020-11-17 17:13:17 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.40462dd018998a47f4c013efcfb92b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452ffb672d1cc498e8e1d833de371e0f",
+    "url": "/chinese-checkers/index.html"
+  },
+  {
+    "revision": "e85e155f85f1f9adf092",
+    "url": "/chinese-checkers/static/css/2.a97e8b61.chunk.css"
+  },
+  {
+    "revision": "111d4b491572a9dfd232",
+    "url": "/chinese-checkers/static/css/main.1bf17e22.chunk.css"
+  },
+  {
+    "revision": "e85e155f85f1f9adf092",
+    "url": "/chinese-checkers/static/js/2.7d99af86.chunk.js"
+  },
+  {
+    "revision": "b896ad03b58c16125479a5bccbaa8e80",
+    "url": "/chinese-checkers/static/js/2.7d99af86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111d4b491572a9dfd232",
+    "url": "/chinese-checkers/static/js/main.d9c50946.chunk.js"
+  },
+  {
+    "revision": "f630fe6977f2d646b6b8",
+    "url": "/chinese-checkers/static/js/runtime-main.ded4409d.js"
+  }
+]);
